600d965128142b765b9edfc7d577fbac96cda45a
SarawadeShrutika <113629567+SarawadeShrutika@users.noreply.github.com>
2022-09-20 01:57:35 -0700

    input value in console
    Taking value from text area.

---
+++ app.js
@@ -0,0 +1,9 @@
+var btn_action=document.querySelector("#btn_transfer");
+var Txt_area=document.querySelector("#Txt_translate");
+
+console.log(Txt_area);
+btn_action.addEventListener("click",function clickEventHandler(){
+    console.log("Clicked!!");
+})
+
+
