8bc719df2066bc30793d906bba50814fd4fa6aa6
SarawadeShrutika <113629567+SarawadeShrutika@users.noreply.github.com>
2022-09-20 01:58:29 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var btn_action=document.querySelector("#btn_transfer");
 var Txt_area=document.querySelector("#Txt_translate");
 
-console.log(Txt_area);
+console.log(Txt_area.value);
 btn_action.addEventListener("click",function clickEventHandler(){
     console.log("Clicked!!");
 })
