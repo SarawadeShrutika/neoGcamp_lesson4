ea064137cf483c0064c7f8c16d27d45a172377e1
SarawadeShrutika <113629567+SarawadeShrutika@users.noreply.github.com>
2022-09-20 02:24:20 -0700

    Input output processing JS
    Input output processing JS

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var btn_action=document.querySelector("#btn_transfer");
 var Txt_area=document.querySelector("#Txt_translate");
-
+var txt_out=document.querySelector("#txt_output");
 console.log(Txt_area.value);
 btn_action.addEventListener("click",function clickEventHandler(){
     console.log("Clicked!!");
+    txt_out.innerText=Txt_area.value;
 })
 
 
